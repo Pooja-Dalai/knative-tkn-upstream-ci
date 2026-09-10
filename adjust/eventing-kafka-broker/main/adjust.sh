fadff03003fe756c0eb00b112919af320727e1b7
Super User <Pooja.Dalai@ibm.com>
2026-09-10 10:58:41 +0000

    updated adjust.sh

--- adjust/eventing-kafka-broker/main/adjust.sh
+++ adjust/eventing-kafka-broker/main/adjust.sh
@@ -6,6 +6,9 @@ sed -i "s/\(go_test_e2e.*\)timeout=20m\(.*\).*/\1timeout=40m\2/g" test/e2e-tests
 # ppc64le.patch is already copied to tmp during setup-environment.sh run
 sed -i "s|K8S_VER_MAJOR|$(echo "$K8S_BUILD_VERSION" | sed -E 's/^v([0-9]+)\.([0-9]+)\..*/\1/')|" /tmp/ppc64le.patch
 sed -i "s|K8S_VER_MINOR|$(echo "$K8S_BUILD_VERSION" | sed -E 's/^v([0-9]+)\.([0-9]+)\..*/\2/')|" /tmp/ppc64le.patch
+
+# Remove unsupported --zap-log-level flag for keda-adapter v2.11.2
+sed -i '/--zap-log-level=error/d' third_party/keda/keda.yaml
 git apply /tmp/ppc64le.patch
 echo "Source code patched successfully"
 
